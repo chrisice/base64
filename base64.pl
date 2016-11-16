eddd4541a01b778c7556fe950fb7a3127a3e2642
Chris Ice <icenhour83@gmail.com>
2016-11-16 13:57:03 -0600

    Create base64.pl

---
+++ base64.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+
+
+use MIME::Base64;
+use Getopt::Long;
+
+my %opts;
+my $encode;
+my $decode;
+
+GetOptions(
+    \%opts,
+	'encode=s' => \$encode,
+	'decode=s' => \$decode,
+);
+
+if ($encode) {
+$string = $encode;
+$out = encode_base64($string);
+print $out;
+}
+elsif ($decode) {
+$string = $decode;
+$out = decode_base64($string);
+print $out;
+}
+else {
+print "Help:\n -encode : Encode a string to base64 \n -decode : Decode a string from base64\n";
+}
