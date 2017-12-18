acf5f3fa410434a097eabe1a24addaf871ad97c3
Chris Ice <icenhour83@gmail.com>
2017-12-18 10:38:18 -0600

    Add newlines to the end of the output

--- base64.pl
+++ base64.pl
@@ -18,11 +18,13 @@ if ($encode) {
 $string = $encode;
 $out = encode_base64($string);
 print $out;
+print "\n"
 }
 elsif ($decode) {
 $string = $decode;
 $out = decode_base64($string);
 print $out;
+print "\n"
 }
 else {
 print "Help:\n -encode : Encode a string to base64 \n -decode : Decode a string from base64\n";
